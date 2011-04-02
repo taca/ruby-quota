8a88488c160d3eec892cc69f255d101c4700f850
taca <taca>
2011-04-02 04:56:52 +0000

    * check <unistd.h> header. * check <sys/statvfs.h> header for NetBSD.

--- extconf.rb
+++ extconf.rb
@@ -7,6 +7,7 @@
 
 require "mkmf"
 
+have_header("unistd.h")
 have_header("linux/quota.h")       # for linux
 have_header("linux/types.h")
 have_header("sys/quota.h")
@@ -16,5 +17,6 @@ have_header("sys/fs/ufs_quota.h")  # for solaris
 
 have_header("ufs/ufs/quota.h")     # for *bsd
 have_header("sys/ucred.h")         # required by FreeBSD and NetBSD
+have_header("sys/statvfs.h")       # required by NetBSD
 
 create_makefile("quota")
